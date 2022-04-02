f261a23e1af36b58f617fde55ba23a883005367d
werslight <siham.tabbaa@etu.unice.fr>
2022-04-02 15:26:02 +0200

    GNGNGN MARCOOO

--- app/src/main/java/etu/unice/revisio/Category.java
+++ app/src/main/java/etu/unice/revisio/Category.java
@@ -1,13 +1,45 @@
 package etu.unice.revisio;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Category extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class Category extends AppCompatActivity implements Parcelable {
+    private String name;
+    private int color;
+    private ArrayList<File> files;
+
+    public Category(String name, int color, ArrayList<File> files) {
+        this.name = name;
+        this.color = color;
+        this.files = files;
+    }
+
+    protected Category(Parcel in) {
+        name = in.readString();
+        color = in.readInt();
+    }
+
+    public static final Creator<Category> CREATOR = new Creator<Category>() {
+        @Override
+        public Category createFromParcel(Parcel in) {
+            return new Category(in);
+        }
+
+        @Override
+        public Category[] newArray(int size) {
+            return new Category[size];
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,9 +53,33 @@ public class Category extends AppCompatActivity {
             startActivity(add);
         });
 
+        addCategory.setOnLongClickListener(view -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Supprimer");
+
+            builder.setMessage("Supprimer la catégorie ...");
+            builder.setPositiveButton("Oui", null);
+            builder.setNegativeButton("Non", null);
+
+            builder.show();
+            return true;
+        });
+
         back.setOnClickListener(view -> {
             Intent back1 = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(back1);
         });
+
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(name);
+        parcel.writeInt(color);
     }
 }
---
+++ app/src/main/java/etu/unice/revisio/ListCategory.java
@@ -0,0 +1,70 @@
+package etu.unice.revisio;
+
+import android.content.Context;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListCategory {
+    ArrayList<Category> list;
+
+    public ListCategory() {
+        list = new ArrayList<Category>();
+    }
+
+    public int size(){
+        return list.size();
+    }
+    public Category get(int pos){
+        return list.get(pos);
+    }
+    public void createList(Context context) {
+        try {
+            JSONArray jsonArray = new JSONArray(getJSONFromAsset(context));
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                list.add(getCategoryFromJSONObject(jsonArray.getJSONObject(i), context));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String getJSONFromAsset(Context context) {
+        String json;
+        try {
+            InputStream is = context.getAssets().open("revision.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
+    private static Category getCategoryFromJSONObject(JSONObject jsonObject, Context context) throws JSONException {
+        String name = jsonObject.getString("name");
+        int color = jsonObject.getInt("color");
+
+        List<File> files = new ArrayList<>();
+        for(int i = 0; i < jsonObject.getJSONArray("files").length(); i++) {
+            File getAllComponent = new File(
+                    jsonObject.getJSONArray("files").getJSONObject(i).getString("name"),
+                    jsonObject.getJSONArray("files").getJSONObject(i).getInt("weight"),
+                    jsonObject.getJSONArray("files").getJSONObject(i).getString("unit")
+         );
+            files.add(getAllComponent);
+        }
+        return new Category();
+    }
+}
