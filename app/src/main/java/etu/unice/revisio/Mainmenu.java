cad30dbe70c75aab3f0c08465d0f4e33a7971932
Lea Cumont <lea.cumont.pro@gmail.com>
2022-04-03 16:14:07 +0200

    j'envois ce que j'ai fait déjà m d r

--- app/src/main/java/etu/unice/revisio/Mainmenu.java
+++ app/src/main/java/etu/unice/revisio/Mainmenu.java
@@ -3,12 +3,69 @@ package etu.unice.revisio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Mainmenu extends AppCompatActivity {
+
+    ImageView logo;
+    TextView title;
+    Button bt1, bt2, bt3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainmenu);
+
+        logo = findViewById(R.id.appLogo);
+        logo.startAnimation(AnimationUtils.loadAnimation(
+                this, R.anim.special));
+
+        title = findViewById(R.id.titleMainMenu);
+        title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(AnimationUtils.loadAnimation(
+                        Mainmenu.this, R.anim.clicked));
+                title.setVisibility(View.VISIBLE);
+            }
+        });
+
+        bt1 = findViewById(R.id.startStudyB);
+        bt2 = findViewById(R.id.editStudyB);
+        bt3 = findViewById(R.id.calendarStudyB);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(AnimationUtils.loadAnimation(
+                        Mainmenu.this, R.anim.clicked));
+                bt1.setVisibility(View.VISIBLE);
+            }
+        });
+
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(AnimationUtils.loadAnimation(
+                        Mainmenu.this, R.anim.clicked));
+                bt2.setVisibility(View.VISIBLE);
+            }
+        });
+
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(AnimationUtils.loadAnimation(
+                        Mainmenu.this, R.anim.clicked));
+                bt3.setVisibility(View.VISIBLE);
+            }
+        });
+
+
     }
 }
