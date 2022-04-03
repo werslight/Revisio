e71849064e9ef0c1a1ae77db799495b12c1a05cb
Lea Cumont <lea.cumont.pro@gmail.com>
2022-04-03 16:42:00 +0200

    design de file edit

--- app/src/main/java/etu/unice/revisio/FileEdit.java
+++ app/src/main/java/etu/unice/revisio/FileEdit.java
@@ -3,12 +3,26 @@ package etu.unice.revisio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 public class FileEdit extends AppCompatActivity {
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_edit);
+
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(AnimationUtils.loadAnimation(
+                        FileEdit.this, R.anim.clicked));
+                back.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
