8e5a5f39a63ff26fa37def501e23916a1d431c88
werslight <siham.tabbaa@etu.unice.fr>
2022-04-03 18:30:44 +0200

    ALLEZ VOUS FAIRE UN CAPPU

--- app/src/main/java/etu/unice/revisio/QuestionsActivity.java
+++ app/src/main/java/etu/unice/revisio/QuestionsActivity.java
@@ -2,8 +2,8 @@ package etu.unice.revisio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -24,6 +24,7 @@ public class QuestionsActivity extends AppCompatActivity {
         RadioButton btn2 = findViewById(R.id.btn2);
         RadioButton btn3 = findViewById(R.id.btn3);
         Button validate = findViewById(R.id.validate);
+        Button lastValidate = findViewById(R.id.lastValidate);
         final String[] getAnswer = new String[1];
         final String[] a1 = new String[1];
         final String[] a2 = new String[1];
@@ -36,88 +37,37 @@ public class QuestionsActivity extends AppCompatActivity {
         btn2.setText(files.getQuestions().get(0).getAnswer2());
         btn3.setText(files.getQuestions().get(0).getAnswer3());
 
-        validate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(btn1.isChecked()) {
-                    getAnswer[0] = btn1.getText().toString();
-                    btn1.setChecked(false);
-                }
+        int i = 0;
 
-                else if(btn2.isChecked()) {
-                    getAnswer[0] = btn2.getText().toString();
-                    btn2.setChecked(false);
-                }
-
-                else if(btn3.isChecked()) {
-                    getAnswer[0] = btn3.getText().toString();
-                    btn3.setChecked(false);
-                }
-
-                a1[0] = getAnswer[0];
-                //answer.setText(a1[0]);
-
-                answer1.setText(files.getQuestions().get(1).getLabel());
-                btn1.setText(files.getQuestions().get(1).getAnswer1());
-                btn2.setText(files.getQuestions().get(1).getAnswer2());
-                btn3.setText(files.getQuestions().get(1).getAnswer3());
-
-                validate.setOnClickListener(view1 -> {
-                    if(btn1.isChecked()) {
-                        getAnswer[1] = "" + btn1.getText().toString();
-                        btn1.setChecked(false);
-                    }
-
-                    else if(btn2.isChecked()) {
-                        getAnswer[1] = "" + btn2.getText().toString();
-                        btn2.setChecked(false);
-                    }
-
-                    else if(btn3.isChecked()) {
-                        getAnswer[1] = "" + btn3.getText().toString();
-                        btn3.setChecked(false);
-                    }
-
-                    a2[0] = getAnswer[1];
-                    //answer.setText(a2[0]);
-
-                    btn1.setChecked(false);
-                    btn2.setChecked(false);
-                    btn3.setChecked(false);
-
-                    answer1.setText(files.getQuestions().get(2).getLabel());
-                    btn1.setText(files.getQuestions().get(2).getAnswer1());
-                    btn2.setText(files.getQuestions().get(2).getAnswer2());
-                    btn3.setText(files.getQuestions().get(2).getAnswer3());
-
-                    validate.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view1) {
-                            if(btn1.isChecked()) {
-                                getAnswer[1] = "" + btn1.getText().toString();
-                                btn1.setChecked(false);
-                            }
-
-                            else if(btn2.isChecked()) {
-                                getAnswer[1] = "" + btn2.getText().toString();
-                                btn2.setChecked(false);
-                            }
-
-                            else if(btn3.isChecked()) {
-                                getAnswer[1] = "" + btn3.getText().toString();
-                                btn3.setChecked(false);
-                            }
-
-                            a3[0] = getAnswer[1];
-                            //answer.setText(a3[0]);
-                           /* Intent result = new Intent(getApplicationContext(), ScoreBoard.class);
-                            startActivity(result);*/
-
-                        }
-                    });
-
-                });
+        validate.setOnClickListener(view -> {
+            if(btn1.isChecked()) {
+                getAnswer[0] = btn1.getText().toString();
+                btn1.setChecked(false);
             }
+
+            else if(btn2.isChecked()) {
+                //getAnswer[0] = btn2.getText().toString();
+                btn2.setChecked(false);
+            }
+
+            else if(btn3.isChecked()) {
+                getAnswer[0] = btn3.getText().toString();
+                btn3.setChecked(false);
+            }
+
+            a1[0] = getAnswer[0];
+            //answer.setText(a1[0]);
+
+            answer1.setText(files.getQuestions().get(i).getLabel());
+            btn1.setText(files.getQuestions().get(i).getAnswer1());
+            btn2.setText(files.getQuestions().get(i).getAnswer2());
+            btn3.setText(files.getQuestions().get(i).getAnswer3());
         });
+
+        lastValidate.setOnClickListener(view -> {
+            Intent intent = new Intent(QuestionsActivity.this, QuestionActivity_2.class);
+            startActivity(intent);
+        });
+
     }
 }
