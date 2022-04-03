97d3423076c444ec749fe3026f19e8d4eb086510
werslight <siham.tabbaa@etu.unice.fr>
2022-04-03 18:31:43 +0200

    ALLEZ VOUS FAIRE UN CAPPU

---
+++ app/src/main/java/etu/unice/revisio/QuestionActivity_2.java
@@ -0,0 +1,15 @@
+package etu.unice.revisio;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuestionActivity_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions);
+
+
+    }
+}
