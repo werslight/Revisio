78b7b409d66b6318e55e9138f7f91714bcb17388
werslight <siham.tabbaa@etu.unice.fr>
2022-04-03 16:45:02 +0200

    radio button selected

--- app/src/main/java/etu/unice/revisio/Application.java
+++ app/src/main/java/etu/unice/revisio/Application.java
@@ -3,4 +3,5 @@ package etu.unice.revisio;
 public class Application {
     public static final String CATEGORY = "category";
     public static final String FILES = "files";
+    public static final String RESULT = "result";
 }
